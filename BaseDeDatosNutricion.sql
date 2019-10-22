87f5f7e0c86edcceb46f2b149b7862eb967333de
Alfredo Osuna <alfredoosuna40@gmail.com>
2019-10-21 20:13:09 -0500

    Base de datos terminada

--- BaseDeDatosNutricion.sql
+++ BaseDeDatosNutricion.sql
@@ -1,3 +1,5 @@
+
+
 DROP DATABASE IF EXISTS Nutricion;
 
 CREATE DATABASE Nutricion;
@@ -8,7 +10,14 @@ use Nutricion;
 -- 
 --
 
-DROP TABLE IF EXISTS Nutricion;
+
+CREATE TABLE Administrador (
+  id int(10)  primary key not null auto_increment,
+  nombre varchar(100) NOT NULL,
+  apellido varchar(100) not null,
+  contraseña varchar(100) not null, 
+  usuario varchar(100) not null
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE table PersonaMayor(
     id int(10) primary key not null auto_increment,
@@ -16,34 +25,33 @@ CREATE table PersonaMayor(
     apellido varchar(100) not null,
     sexo char not null,
     email varchar(100) not null,
-    edad int(3) default 0,
+    edad int(3) DEFAULT 0,
     fechaDeNacimiento DATE not null,
     socioEconomico VARCHAR(100) not null,
     escolaridad varchar(100) not null,
     contraseña varchar(100) not null,
     usuario varchar(100) not null,
     telefono int(12) not null,
-    historiaClinica varbinary not null,
-    id_planNutricional int(10) not NULL,
-    id_historiaClinica int(10) NOT NULL 
-    FOREIGN key(id_planNutricional) REFERENCES PlanNutricional(id)
+    historiaClinica varchar(100) not null,
+    id_planNutricional int(10) NOT NULL,
+    id_historiaClinica int(10) NOT NULL, 
+    FOREIGN key(id_planNutricional) REFERENCES PlanNutricional(id),
     FOREIGN key(id_historiaClinica) REFERENCES HistorialClinico(id)
-
-)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE table RegistroMedico(
     id int(10) primary key not null auto_increment,
-    peso double default 0,
+    peso FLOAT DEFAULT 0,
     fechaRegistro DATE not null,
-    estatura double default 0,
-    observaciones nvarchar(300) not null,
-)
+    estatura FLOAT DEFAULT 0,
+    observaciones nvarchar(300) not null
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE Table Comentarios(
     id int(10) primary key not null auto_increment,
     contenido nvarchar(300) not null,
     fecha DATE not null
-)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE table Nutriologo(
     id int(10) primary key not null auto_increment,
@@ -56,7 +64,7 @@ CREATE table Nutriologo(
     cedula varchar(100) not null,
     domicilioCons varchar(100) not null,
     domicilioPos varchar(100) not null
-)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE PlanNutricional(
 
@@ -65,38 +73,37 @@ CREATE TABLE PlanNutricional(
     recomendaciones VARCHAR(100) not NULL,
     fechaModificacion DATE not null,
     fechaInicio DATE NOT NULL,
-    legumbres DOUBLE DEFAULT 0,
-    lacteos DOUBLE DEFAULT 0,
-    frutas double DEFAULT 0,
-    cereales DOUBLE DEFAULT 0,
-    verduras DOUBLE DEFAULT 0,
-    proteina DOUBLE DEFAULT 0,
-    semillas DOUBLE DEFAULT 0,
-    grasas DOUBLE DEFAULT 0,
-    calendario VARCHAR(100)
+    legumbres FLOAT DEFAULT 0,
+    lacteos FLOAT DEFAULT 0,
+    frutas FLOAT DEFAULT 0,
+    cereales FLOAT DEFAULT 0,
+    verduras FLOAT DEFAULT 0,
+    proteina FLOAT DEFAULT 0,
+    semillas FLOAT DEFAULT 0,
+    grasas FLOAT DEFAULT 0,
+    calendario VARCHAR(100)  
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-    
-)
 CREATE TABLE Comida(
     id int(10) primary key not null auto_increment,
     entrada VARCHAR(100) NOT NULL,
     platoFuerto VARCHAR(100) NOT NULL,
     postre VARCHAR(100) Not NULL,
     bebida varchar(100)
-)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE Seguimiento(
     id int(10) primary key not null auto_increment,
     fechaRegistro DATE NOT NULL,
-    legumbres DOUBLE DEFAULT 0,
-    lacteos DOUBLE DEFAULT 0,
-    frutas double DEFAULT 0,
-    cereales DOUBLE DEFAULT 0,
-    verduras DOUBLE DEFAULT 0,
-    proteina DOUBLE DEFAULT 0,
-    semillas DOUBLE DEFAULT 0,
-    grasas DOUBLE DEFAULT 0,
-)
+    legumbres FLOAT DEFAULT 0,
+    lacteos FLOAT DEFAULT 0,
+    frutas FLOAT DEFAULT 0,
+    cereales FLOAT DEFAULT 0,
+    verduras FLOAT DEFAULT 0,
+    proteina FLOAT DEFAULT 0,
+    semillas FLOAT DEFAULT 0,
+    grasas FLOAT DEFAULT 0
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE HistorialClinico(
     id int(10) primary key not null auto_increment,
@@ -129,7 +136,7 @@ CREATE TABLE HistorialClinico(
     bocaLengua Boolean DEFAULT FALSE,
     bocaLenguaString VARCHAR(100),
     tonoMuscular VARCHAR(100),
-    hallazgos:String DEFAULT FALSE,
+    hallazgos VARCHAR(100),
     otrosString VARCHAR(100),
     medicamentos VARCHAR(100),
     tabaquismo Boolean DEFAULT FALSE,
@@ -142,59 +149,70 @@ CREATE TABLE HistorialClinico(
     drogasFrecuencia VARCHAR(100),
     inactividadSemana VARCHAR(100),
     inactividadFinSemana VARCHAR(100)
-)
---Tablas de relaciones de uno a muchos  y muchos a muchos
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE Nutriologo_PersonaMayor(
     id_nutriologo int (10) not NULL,
     id_personaMayor int (10) not NULL,
     foreign key(id_personaMayor) REFERENCES PersonaMayor(id),
     FOREIGN KEY(id_nutriologo) REFERENCES Nutriologo(id)
-)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 CREATE TABLE Nutriologo_Comentario(
     id_nutriologo int (10) not NULL,
     id_comentario int (10) NOT NULL,
     FOREIGN KEY(id_comentario) REFERENCES Comentario(id),
     FOREIGN KEY(id_nutriologo) REFERENCES Nutriologo(id)
-)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 CREATE TABLE PersonaMayor_Comentario(
     id_personaMayor int (10) not NULL,
     id_comentario int (10) NOT NULL,
     FOREIGN KEY(id_comentario) REFERENCES Comentario(id),
-    Fforeign key(id_personaMayor) REFERENCES PersonaMayor(id)
-)
+    foreign key(id_personaMayor) REFERENCES PersonaMayor(id)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 CREATE TABLE Nutriologo_PlanNutricional(
     id_nutriologo int (10) not NULL,
     id_planNutricional int (10) NOT NULL,
     FOREIGN KEY(id_planNutricional) REFERENCES PlanNutricional(id),
     FOREIGN KEY(id_nutriologo) REFERENCES Nutriologo(id)
     
-)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 CREATE TABLE PersonaMayor_Seguimiento(
     id_personaMayor int (10) not NULL,
     id_seguimiento int (10) NOT NULL,
     FOREIGN KEY(id_seguimiento) REFERENCES Seguimiento(id),
-    Fforeign key(id_personaMayor) REFERENCES PersonaMayor(id)
-)
+    foreign key(id_personaMayor) REFERENCES PersonaMayor(id)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 CREATE TABLE PersonaMayor_RegistroMedico(
     id_personaMayor int (10) not NULL,
     id_registroMedico int (10) NOT NULL,
     FOREIGN KEY(id_registroMedico) REFERENCES RegistroMedico(id),
-    Fforeign key(id_personaMayor) REFERENCES PersonaMayor(id)
-)
+    foreign key(id_personaMayor) REFERENCES PersonaMayor(id)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE Comida_PlanNutricional(
     id_comida int (10) not NULL,
     id_planNutricional int (10) NOT NULL,
     FOREIGN KEY(id_planNutricional) REFERENCES PlanNutricional(id),
-    FOREIGN KEY(id_nutriologo) REFERENCES Comida(id)
+    FOREIGN KEY(id_comida) REFERENCES Comida(id)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
     
-)
 
 DROP USER 'nutricion'@'localhost';
 flush privileges;
 
-INSERT INTO Usuario (nombre,contraseña,usuario) VALUES ('administrador', 'admin1234', 'admin');
+INSERT INTO Administrador (nombre, apellido,contraseña,usuario) VALUES ('administrador','admin', 'admin1234', 'admin');
 
 CREATE USER 'nutricion'@'localhost' IDENTIFIED BY 'mipass';
 GRANT SELECT, UPDATE, INSERT, DELETE ON Nutricion.* TO 'nutricion'@'localhost';
+
+
+
+
+
+
+
